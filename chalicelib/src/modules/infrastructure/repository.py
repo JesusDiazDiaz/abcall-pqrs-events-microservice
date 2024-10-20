7c6ec9c88f045db612f4aa2de6788a9d7543c42b
Jesus Diaz <jesus.diaz@moonshot.partners>
2024-10-19 19:51:24 -0500

    feat: Correct models

--- chalicelib/src/modules/infrastructure/repository.py
+++ chalicelib/src/modules/infrastructure/repository.py
@@ -24,21 +24,20 @@ class IncidenceRepositoryPostgres(IncidenceRepository):
         estimated_close_date = incidence_date + timedelta(days=8)
 
         LOGGER.info(f"Get user by sub {incidence.user_sub}")
-        # TODO: Add retrieve user
-        # facade = MicroservicesFacade()
-        # current_user = facade.get_user(incidence.user_sub)
+        facade = MicroservicesFacade()
+        current_user = facade.get_user(incidence.user_sub)
 
         try:
             incidence = Incidence(
-                client_id='1',
+                client_id=current_user.client_id,
                 subject=incidence.title,
                 description=incidence.description,
                 status=Status.ABIERTO,
                 date=incidence_date,
                 estimated_close_date=estimated_close_date,
                 user_sub=incidence.user_sub,
-                type=IncidentType.PETICION,
-                type_communication=CommunicationType.EMAIL
+                type=IncidentType(IncidentType.PETICION),
+                communication_type=CommunicationType(current_user.communication_type)
             )
 
             self.db_session.add(incidence)
