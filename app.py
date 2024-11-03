6f1e4d06a1cc573faa82c465887b18ff9d165c43
Jesus Diaz <jesus.diaz@moonshot.partners>
2024-11-03 14:02:01 -0500

    chore: Change to sqs

--- app.py
+++ app.py
@@ -10,20 +10,21 @@ app.log.setLevel(logging.DEBUG)
 LOGGER = logging.getLogger('abcall-pqrs-events-microservice')
 
 
-@app.on_sns_message(topic='AbcallPqrsTopic')
-def handle_sns_message(event):
+# @app.on_sns_message(topic='AbcallPqrsTopic')
+@app.on_sqs_message(queue='AbcallPqrs', batch_size=1)
+def handle_sqs_message(event):
     app.log.debug("Received message with subject: %s, message: %s",
                   event.subject, event.message)
 
-    incidence_as_json = json.loads(event.message)
+    for record in event:
+        incidence_as_json = json.loads(record.body)
 
-    command = CreateIncidenceCommand(
-        title=incidence_as_json["title"],
-        type=incidence_as_json["type"],
-        description=incidence_as_json["description"],
-        date=incidence_as_json["date"],
-        user_sub=incidence_as_json["user_sub"],
-        ticket_number=incidence_as_json["ticket_number"],
-    )
-
-    execute_command(command)
+        command = CreateIncidenceCommand(
+            title=incidence_as_json["title"],
+            type=incidence_as_json["type"],
+            description=incidence_as_json["description"],
+            date=incidence_as_json["date"],
+            user_sub=incidence_as_json["user_sub"],
+            ticket_number=incidence_as_json["ticket_number"],
+        )
+        execute_command(command)
